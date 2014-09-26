5fbafff04a0668b94633a7272bfcf42a8e9714cd
Oliver Charles Ahlberg <oliver@charlesweb.se>
2014-09-26 14:22:03 +0200

    Lagt in koppling till databasen
    Nu skriver den ut alla produkter.

--- VitVaruButik/VitVaruButik/VitVaruButik/Mainform.Designer.cs
+++ VitVaruButik/VitVaruButik/VitVaruButik/Mainform.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(588, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(466, 389);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.List;
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 413);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.button1);
+            this.Name = "Mainform";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView;
     }
 }
 
